7ece79e73c8cf0d8e6adfe5451a3ea9b89440b42
OrigamiKing3612 <origamiking3612@gmail.com>
2024-01-25 14:36:57 -0600

    Initial Commit

---
+++ Thesaurus App/Thesaurus_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Thesaurus_AppApp.swift
+//  Thesaurus App
+//
+//  Created by Malachi on 1/25/24.
+//
+
+import SwiftUI
+
+@main
+struct Thesaurus_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
